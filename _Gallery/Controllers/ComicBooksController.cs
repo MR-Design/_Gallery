ccc33954a39310a6620da2899ff656f98315bcdf
MR-Design <two7city@gmail.comMR-Design>
2018-10-23 15:26:14 -0500

    Added comic books controller

---
+++ _Gallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace _Gallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+
+    }
+}
